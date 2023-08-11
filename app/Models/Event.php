f8f1bcaddd3cd4b1fcbbc64262ed5dbf98555d94
wuwx <wuweixin@gmail.com>
2023-08-11 13:33:42 +0800

    Create Some Models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Event\Session;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+
+    public function sessions(): HasMany
+    {
+        return $this->hasMany(Session::class);
+    }
 }
