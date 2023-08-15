c0198b443b1c2e731342b40a909f5393a2a3addf
wuwx <wuweixin@gmail.com>
2023-08-15 09:24:10 +0800

    Associate user from Auth::user()

--- app/Http/Controllers/RegistrantController.php
+++ app/Http/Controllers/RegistrantController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRegistrantRequest;
 use App\Models\Event;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegistrantController extends Controller
 {
     public function store(Event $event, StoreRegistrantRequest $request)
     {
         $registrant = $event->registrants()->make($request->all());
-        $registrant->user()->associate(User::first());
+        $registrant->user()->associate(Auth::user());
         $registrant->save();
     }
 }
