2b417b98df0c421cbc24136082a3bdf0feb5dacd
wuwx <wuweixin@gmail.com>
2023-08-16 17:54:33 +0800

    Add Event Policies

---
+++ app/Policies/Event/AttendeePolicy.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Policies\Event;
+
+use App\Models\User;
+use App\Models\Event\Attendee;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AttendeePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Attendee $attendee): bool
+    {
+        return $user->can('view_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Attendee $attendee): bool
+    {
+        return $user->can('update_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Attendee $attendee): bool
+    {
+        return $user->can('delete_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Attendee $attendee): bool
+    {
+        return $user->can('force_delete_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Attendee $attendee): bool
+    {
+        return $user->can('restore_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Event\Attendee  $attendee
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function replicate(User $user, Attendee $attendee): bool
+    {
+        return $user->can('replicate_event::attendee');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_event::attendee');
+    }
+
+}
