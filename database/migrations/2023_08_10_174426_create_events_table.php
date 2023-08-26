4ae9f7722ccd979c15e00184781d367472ae9d03
wuwx <wuweixin@gmail.com>
2023-08-27 07:03:53 +0800

    add some fields

--- database/migrations/2023_08_10_174426_create_events_table.php
+++ database/migrations/2023_08_10_174426_create_events_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
+            $table->string('timezone');
+            $table->string('type');
+            $table->string('access_level');
             $table->timestamps();
         });
     }
--- database/migrations/2023_08_11_044959_create_event_ticket_types_table.php
+++ database/migrations/2023_08_11_044959_create_event_ticket_types_table.php
@@ -15,7 +15,14 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('event_id');
             $table->string('name');
+            $table->string('currency');
+            $table->boolean('free');
+            $table->string('price');
+            $table->integer('quantity');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
             $table->text('description');
+            $table->integer('sold_quantity');
             $table->timestamps();
         });
     }
