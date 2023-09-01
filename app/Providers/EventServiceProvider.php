310e6fe56d85ee27d668d3c74a9946097926df61
wuwx <wuweixin@gmail.com>
2023-09-01 15:46:16 +0800

    new Notification

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Events\EventTicketCreated;
+use App\Notifications\EventTicketCreatedNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Request;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +28,9 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(function (EventTicketCreated $event) {
+            Request::user()->notify(new EventTicketCreatedNotification());
+        });
     }
 
     /**
