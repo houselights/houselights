4fbf72676836cda7aec47df10e699615fef570d5
wuwx <wuweixin@gmail.com>
2023-09-05 11:25:28 +0800

    auto discover

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use App\Models;
-use App\Policies;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,11 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Models\Event\Session::class => Policies\Event\SessionPolicy::class,
-        Models\Event\TicketType::class => Policies\Event\TicketTypePolicy::class,
-        Models\Event\Registrant::class => Policies\Event\RegistrantPolicy::class,
-        Models\Event\Ticket::class => Policies\Event\TicketPolicy::class,
-        Models\Event\Attendee::class => Policies\Event\AttendeePolicy::class,
+        //
     ];
 
     /**
