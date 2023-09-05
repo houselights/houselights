66a3085782f4c2b3b4862576d9928fe14a196eb9
wuwx <wuweixin@gmail.com>
2023-09-05 11:44:48 +0800

    manually registering button and dropdown

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\View\Components\Button;
+use App\View\Components\Dropdown;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component('dropdown', Dropdown::class);
+        Blade::component('button', Button::class);
     }
 }
